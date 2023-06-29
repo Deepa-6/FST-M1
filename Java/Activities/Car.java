cea4dc318d01904bea9886dc7436038ba507705a
Deepa-6 <137272591+Deepa-6@users.noreply.github.com>
2023-06-29 15:51:55 +0530

    Add files via upload

---
+++ Java/Activities/Car.java
@@ -0,0 +1,32 @@
+package activities;
+
+public class Car {
+    String color;
+    int make;
+    String transmission;
+    int tyres;
+    int doors;
+
+    Car() {
+        tyres = 4;
+        doors = 4;
+    }
+    public void displayCharacterstics(){
+        System.out.println("Color of the car" +color);
+        System.out.println("Make of the car" +make);
+        System.out.println("Transmission of the car" +transmission);
+        System.out.println("Number of the doors on the car" +doors);
+        System.out.println("Number of the tyres on the car" +tyres);
+    }
+
+    public void accelerate() {
+        System.out.println("Car is moving forward");
+    }
+
+    public void brake() {
+        System.out.println("Car has stopped");
+    }
+
+
+    }
+
